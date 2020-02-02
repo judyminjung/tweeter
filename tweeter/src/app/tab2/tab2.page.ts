ac640bf6542f41768f2a6df994b144d7b2ba4a3d
judyminjung <minjungs@usc.edu>
2020-02-01 18:23:44 -0800

    ng2-charts needs work

--- tweeter/src/app/tab2/tab2.page.ts
+++ tweeter/src/app/tab2/tab2.page.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../services/message.service';
+import { Message } from '../models/message';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  messages: Message[];
+  message: Message;
 
+  // Chart fields
+  graphDelimeter = 10;
+  graphOptions = {
+    responsive: true
+  }
+  graphTitle = "Messages over time";
+  graphType = "bar";
+  graphData = [{ data: [], label: 'Message scores' }];
+  graphLabels = [];
+  graphLegend = true;
+
+
+  constructor(private messageService: MessageService) {
+    this.message = new Message();
+    this.messages = [];
+  }
+
+  ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+    this.getMessages();
+  }
+
+  getMessages() {
+    this.messageService.getMessages().subscribe(data => {
+      this.messages = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          ...e.payload.doc.data()
+        } as Message;
+      });
+      let obj = this.messageService.getGraphDataAndLabels(this.messages);
+      this.graphData = obj.graphData;
+      this.graphLabels = obj.graphLabels;
+    });
+  }
 }
